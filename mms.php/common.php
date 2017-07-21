294a9728197f8991d06709aae9191446fe2732ff
henryZhaobo <30336529+henryZhaobo@users.noreply.github.com>
2017-07-21 15:40:35 +0800

    创建mms

---
+++ mms.php/common.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+error_reporting(E_ALL^E_NOTICE);
+date_default_timezone_set("PRC");
+try{
+    $pdo=new PDO("mysql:host=localhost;dbname=web13","root","");
+}catch (PDOException $e){
+    echo $e->getMessage();
+}
+$pdo->query("set names utf8");
+?>
